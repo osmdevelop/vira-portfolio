d8a38000ce58425d544e3a7138c1c94cd099a664
Oleh Smolikevych <66628075+osmdevelop@users.noreply.github.com>
2024-06-23 18:38:04 -0500

    initial

---
+++ src/views/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Navbar from "../../components/Navbars/ScrollTransparentNavbar"
+import Header from "../../components/Header"
+
+const HomePage = () => {
+    return (
+        <div>
+            <Header />
+            text
+        </div>
+    );
+};
+
+export default HomePage;
